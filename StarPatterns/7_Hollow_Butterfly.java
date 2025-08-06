1f59c4568b1b67a53c90cceaaa1d3317e5498bf3
Varsha V Nayak <153977682+Varsha0818@users.noreply.github.com>
2025-08-06 15:48:35 +0530

    Update 7_Hollow_Butterfly.java

--- StarPatterns/7_Hollow_Butterfly.java
+++ StarPatterns/7_Hollow_Butterfly.java
@@ -10,55 +10,54 @@
 **      **
 *        *
 
-  public class StarPattern7 {
-    public static void main(String args[]){
-        int n = 5;
-
-        //Upper Half
-        for(int i=1;i<=n;i++){
-            //1st Part
-            for(int j=1;j<=n;j++){
-                if(i==j || j==1){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            //2nd Part
-            for(int j=n;j>=1;j--){
-                if(i==j || j==1){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+public class StarPattern7 {
+  public static void main(String args[]){
+    int n = 5;
+    
+    //Upper Half
+    for(int i=1;i<=n;i++){
+      //1st Part
+      for(int j=1;j<=n;j++){
+        if(i==j || j==1){
+          System.out.print("*");
         }
-
-        //Lower Half
-        for(int i=n;i>=1;i--){
-            //1st Part
-            for(int j=1;j<=n;j++){
-                if(i==j || j==1){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            //2nd Part
-            for(int j=n;j>=1;j--){
-                if(i==j || j==1){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        else{
+          System.out.print(" ");
         }
-
+      }
+      //2nd Part
+      for(int j=n;j>=1;j--){
+        if(i==j || j==1){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
     }
+    //Lower Half
+    for(int i=n;i>=1;i--){
+      //1st Part
+      for(int j=1;j<=n;j++){
+        if(i==j || j==1){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      //2nd Part
+      for(int j=n;j>=1;j--){
+        if(i==j || j==1){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
 }
+               
