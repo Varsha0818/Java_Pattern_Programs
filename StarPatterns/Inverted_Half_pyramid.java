b088dbc4666e5f00b30ed32d65d7f02c06d5f0ad
Varsha V Nayak <153977682+Varsha0818@users.noreply.github.com>
2025-08-06 15:16:23 +0530

    Create Inverted_Half_pyramid.java

---
+++ StarPatterns/Inverted_Half_pyramid.java
@@ -0,0 +1,19 @@
+/* Print the Pattern- Half Pyramid
+****
+***
+**
+*
+*/
+
+public class StarPattern4 {
+    public static void main(String args[]){
+        int n = 4;        
+
+        for(int i=n; i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
